6d64fe83bc11d6ecc587643c85d8226531703fa2
Nathan Zadoks <nathan@nathan7.eu>
2016-02-02 00:01:38 +0100

    add GTK icons (hicolor)

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,8 @@ rec {
   environment.variables.EDITOR = "nvim";
   environment.systemPackages = with pkgs; let
     sshttp = pkgs.callPackage ./pkgs/sshttp {};
-  in [ xorg.xbacklight python jdk aws nixops evince pythonPackages.pgcli psmisc arandr thunderbird mpv transmission glxinfo xonotic ghc vim nodejs fish chromium neovim terminator nix-repl silver-searcher which mosh compton git pass gnupg sshttp ctags editorconfig-core-c alsaUtils whois xorg.xf86inputsynaptics htop pv taskwarrior file gnome3.eog unzip jq git-hub pkgs.boot libreoffice atom skype wget spotify steam ];
+    gtk-icons = pkgs.hicolor_icon_theme;
+  in [ xorg.xbacklight python jdk aws nixops evince pythonPackages.pgcli psmisc arandr thunderbird mpv transmission glxinfo xonotic ghc vim nodejs fish chromium neovim terminator nix-repl silver-searcher which mosh compton git pass gnupg sshttp ctags editorconfig-core-c alsaUtils whois xorg.xf86inputsynaptics htop pv taskwarrior file gnome3.eog unzip jq git-hub pkgs.boot libreoffice atom skype wget spotify steam gtk-icons ];
 
   services.xserver = {
     enable = true;
