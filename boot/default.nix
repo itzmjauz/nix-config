f338b0d737df0962193679f17a97177ae38cca0e
Nathan Zadoks <nathan@nathan7.eu>
2016-04-27 14:15:16 -0400

    switch gummibootr and systemd-zfs-generator to import-from-derivation

--- boot/default.nix
+++ boot/default.nix
@@ -4,7 +4,19 @@ let
   poolUUID = "4ddb1208-d1c9-4e72-bfdc-da4a439ed6d6";
 in
 {
-  imports = [ ./systemd-zfs-generator ./gummibootr ];
+  imports = let
+    pkgs = import <nixpkgs> {};
+    systemd-zfs-generator = pkgs.fetchcode {
+      repo = "systemd-zfs-generator";
+      rev = "80b3a2daf23a0d9abd2e1b1642d1064a7f875397";
+      sha256 = "0qm7cvd91pj4q7v7yjzr2wqqdnzm6iqkjipd63k4676hjcmlkmfy";
+    };
+    gummibootr = pkgs.fetchcode {
+      repo = "gummibootr";
+      rev = "826cba3609459deeef4c34f126135118e79b2c55";
+      sha256 = "0bjjc8s2ss2wh260hqzimba6swy0jw0szpj91pv3sq7dzbpradsm";
+    };
+  in builtins.map (drv: drv.outPath) [ systemd-zfs-generator gummibootr ];
 
   networking.hostId = "a8c09ec5";
 
--- boot/systemd-zfs-generator/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  systemd.generators.systemd-zfs-generator = pkgs.substituteAll {
-    name = "systemd-zfs-generator";
-    src = ./systemd-zfs-generator.sh;
-    inherit (pkgs) bash gawk zfs systemd busybox;
-    isExecutable = true;
-  };
-}
