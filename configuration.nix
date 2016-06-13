3fcb395107330fc558bbfe892ca1974496ad0bd9
Nathan Zadoks <nathan@nathan7.eu>
2016-06-13 17:51:50 -0400

    remove sshttp

--- configuration.nix
+++ configuration.nix
@@ -10,9 +10,8 @@ rec {
 
   environment.variables.EDITOR = "nvim";
   environment.systemPackages = with pkgs; let
-    sshttp = pkgs.callPackage ./pkgs/sshttp {};
     gtk-icons = pkgs.hicolor_icon_theme;
-  in [ xorg.xbacklight python jdk aws nixops evince pythonPackages.pgcli psmisc arandr thunderbird mpv transmission glxinfo xonotic ghc vim nodejs fish chromium neovim terminator nix-repl silver-searcher which mosh compton git pass gnupg sshttp ctags editorconfig-core-c alsaUtils whois xorg.xf86inputsynaptics htop pv taskwarrior file gnome3.eog unzip jq git-hub pkgs.boot libreoffice atom skype wget spotify steam gtk-icons ];
+  in [ xorg.xbacklight python jdk aws nixops evince pythonPackages.pgcli psmisc arandr thunderbird mpv transmission glxinfo xonotic ghc vim nodejs fish chromium neovim terminator nix-repl silver-searcher which mosh compton git pass gnupg ctags editorconfig-core-c alsaUtils whois xorg.xf86inputsynaptics htop pv taskwarrior file gnome3.eog unzip jq git-hub pkgs.boot libreoffice atom skype wget spotify steam gtk-icons ];
 
   services.xserver = {
     enable = true;
