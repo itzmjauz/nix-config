ce70fe02561be75c4c8125b300f702b8720a7bdc
Nathan Zadoks <nathan@nathan7.eu>
2016-08-17 13:39:46 +0200

    upgrade kernel to 4.4

--- kernel.nix
+++ kernel.nix
@@ -1,33 +1,31 @@
 { config, pkgs, ... }:
+let
+  patchf = attrs: {
+    name = attrs.name or (baseNameOf attrs.url);
+    patch = pkgs.fetchurl attrs;
+  };
+  linux = pkgs.linux_4_4.override {
+    extraConfig = ''
+      IOSCHED_BFQ y
+    '';
+    kernelPatches = builtins.map patchf [
+      {
+        url = http://algo.ing.unimo.it/people/paolo/disk_sched/patches/4.4.0-v7r11/0001-block-cgroups-kconfig-build-bits-for-BFQ-v7r11-4.4.0.patch;
+        sha256 = "1kmlfz63610zc4lxhanjsn4hhw43cdsbk3pyaij723vbd7619kyi";
+      }
+      {
+        url = http://algo.ing.unimo.it/people/paolo/disk_sched/patches/4.4.0-v7r11/0002-block-introduce-the-BFQ-v7r11-I-O-sched-for-4.4.0.patch;
+        sha256 = "1i5jqkxglp3ah76i4vyi13pnmjkr6qlqy69qbaj2132vijqkyz5i";
+      }
+      {
+        url = http://algo.ing.unimo.it/people/paolo/disk_sched/patches/4.4.0-v7r11/0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-v7r11-for.patch;
+        sha256 = "09bv31s8d2aphi3d9py4sz1gcvyb5645a8s7zj614a56hv11p8k9";
+      }
+    ];
+  };
+  linuxPackages = pkgs.linuxPackagesFor linux linuxPackages;
+in
 {
-  boot.kernelPackages = let
-    patchf = attrs: {
-      name = if attrs ? name
-        then attrs.name
-        else baseNameOf attrs.url;
-      patch = pkgs.fetchurl attrs;
-    };
-    linux = pkgs.linux_4_3.override {
-      extraConfig = ''
-        IOSCHED_BFQ y
-        CGROUP_BFQIO y
-      '';
-      kernelPatches = builtins.map patchf [
-        {
-          url = http://algo.ing.unimo.it/people/paolo/disk_sched/patches/4.3.0-v7r8/0001-block-cgroups-kconfig-build-bits-for-BFQ-v7r8-4.3.patch;
-          sha256 = "14549awmvsqwzb7912k83dmlznx5lak8gcivqjd79clrd4h65szb";
-        }
-        {
-          url = http://algo.ing.unimo.it/people/paolo/disk_sched/patches/4.3.0-v7r8/0002-block-introduce-the-BFQ-v7r8-I-O-sched-for-4.3.patch;
-          sha256 = "1sw65hxjimg9w04f7ccrxhl1c8b1ddd21ni9af77cyrkp11cpdwi";
-        }
-        {
-          url = http://algo.ing.unimo.it/people/paolo/disk_sched/patches/4.3.0-v7r8/0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-v7r8-for-4.3.0.patch;
-          sha256 = "178spkcwaz79rz8fb7h209grbgr2ca4lbgfmns4d43b7aiqhqhvp";
-        }
-      ];
-    };
-    linuxPackages = pkgs.linuxPackagesFor linux linuxPackages;
-  in linuxPackages;
+  boot.kernelPackages = linuxPackages;
   boot.kernelParams = [ "elevator=bfq" ];
 }
