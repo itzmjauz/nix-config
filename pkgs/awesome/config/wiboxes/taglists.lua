150298f37fb0c4b5d4cbc06755f47cd0fb7f0dab
Antoni Stevenet <itzmjauz@gmail.com>
2018-06-08 17:00:32 +0200

    remove deprecated files

--- pkgs/awesome/config/wiboxes/taglists.lua
+++
@@ -1,8 +0,0 @@
-local awful = require('awful')
-local buttons = require('bindings.global.tags').buttons
-
-awful.screen.connect_for_each_screen(function(s)
-  s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, buttons)
-end)
-
-return true
--- pkgs/awesome/config/wiboxes/tasklists.lua
+++
@@ -1,9 +0,0 @@
-local awful = require('awful')
-local buttons = require('bindings.global.tasks').buttons
-
-awful.screen.connect_for_each_screen(function(s)
-  s.tasklists = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, buttons)
-end)
-
-return true
-
