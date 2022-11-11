daf1396d65693ccfdb87589677824267598728b0
Antoni Stevenet <itzmjauz@gmail.com>
2022-11-11 14:53:52 +0100

    update overlay for nvim 0.8 and libvterm 0.3

--- overlays/neovim.nix
+++ overlays/neovim.nix
@@ -1,10 +1,19 @@
 self: super: {
+  libvterm-neovim = super.libvterm-neovim.overrideAttrs (oldAttrs : {
+    version = "0.3";
+    src = (builtins.fetchTarball https://www.leonerd.org.uk/code/libvterm/libvterm-0.3.tar.gz);
+    nativeBuildInputs = super.libvterm.nativeBuildInputs ++ [self.perl self.libtool];
+  });
+
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (oldAttrs: {
     version = "master";
     src = builtins.fetchGit {
       url = https://github.com/neovim/neovim.git;
-      rev = "e65b724451ba5f65dfcaf8f8c16afdd508db7359";
+      ref = "release-0.8";
+      rev = "81781810e6e1c3538c2cd3e220b30b81bbdd0362";
     };
-    nativeBuildInputs = super.neovim-unwrapped.nativeBuildInputs ++ [ super.tree-sitter self.cmake self.binutils-unwrapped ];
+    nativeBuildInputs = super.neovim-unwrapped.nativeBuildInputs ++ [ super.tree-sitter self.cmake self.binutils-unwrapped self.libvterm-neovim];
   });
+
+  
 }
